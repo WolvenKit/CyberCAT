d81d9cb6609d8a704c4c6d217962d0e7f7ad8cca
Mathies Föls <mathiesfoels@gmail.com>
2020-12-28 17:24:40 +0100

    Partly fixed Headersize problems added stub VirtualNodeEntry.cs

---
+++ CyberCAT.Forms/Classes/VirtualNodeEntry.cs
@@ -0,0 +1,15 @@
+using CyberCAT.Core.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CyberCAT.Forms.Classes
+{
+    class VirtualNodeEntry : NodeEntry
+    {
+       
+       
+    }
+}
