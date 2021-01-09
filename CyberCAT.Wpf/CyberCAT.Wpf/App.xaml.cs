3a56175cbd9a4589a36728c2e81f3ca589936f74
SirBitesalot <SirBitesalot@ez1.eu>
2021-01-09 19:07:45 +0100

    Added automatically retrieved 3rd party licenses

---
+++ CyberCAT.Wpf/CyberCAT.Wpf/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using ControlzEx.Theming;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CyberCAT.Wpf
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            // Set the application theme to Dark.Green
+            ThemeManager.Current.ChangeTheme(this, "Dark.Steel");
+
+        }
+    }
+}
