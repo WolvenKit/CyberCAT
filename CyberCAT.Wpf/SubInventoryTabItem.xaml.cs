fc63afb4b9bf75a24ad0c983ef108fee6ed1df55
Nico Weichbrodt <git@nico.weichbrodt.me>
2021-01-16 23:15:26 +0100

    Make it possible to move items between inventories

--- CyberCAT.Wpf/SubInventoryTabItem.xaml.cs
+++ CyberCAT.Wpf/SubInventoryTabItem.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -10,8 +11,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using CyberCAT.Core;
 using CyberCAT.Core.Classes;
 using CyberCAT.Core.Classes.NodeRepresentations;
+using CyberCAT.Wpf.Classes;
 using MahApps.Metro.Controls;
 
 namespace CyberCAT.Wpf
@@ -23,17 +26,44 @@ namespace CyberCAT.Wpf
     {
         public SaveFile SaveFile { get; }
 
-        private static Dictionary<ulong, string> InventoryNames = new Dictionary<ulong, string>
+        private Inventory.SubInventory ThisInventory { get; }
+
+        private static readonly Dictionary<ulong, string> InventoryNames = new Dictionary<ulong, string>
         {
             { 0x1, "V's Bag" },
             { 0xF4240, "Car Stash" },
+            { 0x895724, "Nomad Intro Items" },
+            { 0x895956, "Street Kid Intro Items" },
+            { 0x8959E8, "Corpo Intro Items" },
+            { 0x38E8D0C9F9A087AE, "Judy's Stash" },
+            { 0x7901DE03D136A5AF, "V's Wardrobe" },
             { 0xE5F556FCBB62A706, "V's Stash" }
         };
 
         public SubInventoryTabItem(Inventory.SubInventory subInventory, SaveFile saveFile)
         {
+            ThisInventory = subInventory;
             SaveFile = saveFile;
             InitializeComponent();
+
+            var inventories = saveFile.GetInventory().SubInventories;
+            foreach (var si in inventories)
+            {
+                if (si == ThisInventory)
+                {
+                    continue;
+                }
+                var mi = new MenuItem {
+                    Header = new TextBlock
+                    {
+                        Text = InventoryNames.TryGetValue(si.InventoryId, out var siName) ? siName : $"{si.InventoryId:X}"
+                    },
+                    Tag = si,
+                };
+                mi.Click += OnMoveToInventoryClicked;
+                MoveToMenu.Items.Add(mi);
+            }
+
             Header = InventoryNames.TryGetValue(subInventory.InventoryId, out var name) ? name : $"{subInventory.InventoryId:X}";
             Items.ItemsSource = subInventory.Items;
             var itemsView = CollectionViewSource.GetDefaultView(Items.ItemsSource);
@@ -59,6 +89,14 @@ namespace CyberCAT.Wpf
         private void SelectedItemChanged(object sender, SelectionChangedEventArgs e)
         {
             ItemEditor.Item = (ItemData)Items.SelectedItem;
+        }
+
+        private void OnMoveToInventoryClicked(object sender, RoutedEventArgs e)
+        {
+            var targetInventory = (Inventory.SubInventory)((MenuItem)sender).Tag;
+            var item = (ItemData)Items.SelectedItem;
+            ThisInventory.Items.Remove(item);
+            targetInventory.Items.Add(item);
         }
     }
 }
