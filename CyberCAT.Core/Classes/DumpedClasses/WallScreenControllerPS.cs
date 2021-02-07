cc396d3461a515f7dc15a2b0237ffbc7768a9131
Marcel Richter <seberoth@bg-entwicklung.de>
2021-02-07 19:40:30 +0100

    Missing game class

---
+++ CyberCAT.Core/Classes/DumpedClasses/WallScreenControllerPS.cs
@@ -0,0 +1,11 @@
+﻿using CyberCAT.Core.Classes.Mapping;
+
+namespace CyberCAT.Core.Classes.DumpedClasses
+{
+    [RealName("WallScreenControllerPS")]
+    public class WallScreenControllerPS : TVControllerPS
+    {
+        [RealName("isShown")]
+        public bool IsShown { get; set; }
+    }
+}
